21590abef5ff70580f825b76553ebf60917e7a83
Andy Gonzalez <andy.gonzalez@sap.com>
2017-12-21 16:00:41 -0500

    Add some sample data to our partials using an angular controller

--- public/app/main/mvMainCtrl.js
+++ public/app/main/mvMainCtrl.js
@@ -1,3 +1,24 @@
 angular.module('app').controller('mvMainCtrl', function($scope) {
-  $scope.myVar = "Hello Angular";
+  $scope.courses = [
+    {
+      name: 'C# for sociopaths',
+      featured: true,
+      published: new Date('2017-07-26')
+    },
+    {
+      name: 'Super duper expert C#',
+      featured: false,
+      published: new Date('2017-01-01')
+    },
+    {
+      name: 'JavaScript for people over 20',
+      featured: true,
+      published: new Date('2017-12-8')
+    },
+    {
+      name: 'Code reviews for jerks',
+      featured: false,
+      published: new Date('2016-04-01')
+    }
+  ];
 });
