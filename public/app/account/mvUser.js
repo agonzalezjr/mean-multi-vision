7a8bd2545597973e1d90ea00d2d7c6fa41a25a33
Andy Gonzalez <andy.gonzalez@sap.com>
2018-01-05 13:01:02 -0500

    minor cleanups

--- public/app/account/mvUser.js
+++ public/app/account/mvUser.js
@@ -1,7 +1,10 @@
 angular.module('app').factory('mvUser', function($resource) {
   
   var UserResource = $resource('/api/users/:id', {_id: "@id"}, {
-    update: {method:'PUT',isArray:false}
+    update: {
+      method:'PUT',
+      isArray:false
+    }
   });
 
   UserResource.prototype.isAdmin = function() {
