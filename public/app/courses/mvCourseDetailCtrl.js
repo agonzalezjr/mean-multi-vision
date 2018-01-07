188942145cdb30223b012c4f6471b77f5c4d56fa
Andy Gonzalez <andy.gonzalez@sap.com>
2018-01-07 14:10:23 -0500

    Course detail page linking

--- public/app/courses/mvCourseDetailCtrl.js
+++ public/app/courses/mvCourseDetailCtrl.js
@@ -1,4 +1,5 @@
 angular.module('app').controller('mvCourseDetailCtrl', function($scope, mvCachedCourses, $routeParams) {
+
   mvCachedCourses.query().$promise.then(function(collection) {
     collection.forEach(function(course) {
       if(course._id === $routeParams.id) {
@@ -6,4 +7,5 @@ angular.module('app').controller('mvCourseDetailCtrl', function($scope, mvCached
       }
     });
   });
+
 });
